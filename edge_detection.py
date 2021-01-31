8dbec1578485ed116bd5f4b80a3058833598bc84
buntech <yu.sskabc.my123@gmail.com>
2021-01-31 13:11:15 +0900

    リスト転置

--- edge_detection.py
+++ edge_detection.py
@@ -1,13 +1,16 @@
+# %matplotlib inline
 import matplotlib.pyplot as plt
 import numpy as np
-%matplotlib inline
-
 
 # define the vertical filter
 vertical_filter = [[-1, 2, 1], [0, 0, 0], [1, 2, 1]]
 
 # define the horizontal filter
 horizontal_filter = [[-1, 0, 1], [-2, 0, 2], [1, 2, 1]]
+
+# リストは転置できないので、[ndarray]や[pandas]に変換 or zip() 使用
+# horizontal_filter_T = vertical_filter.T
+
 
 # read in the pinwheel image
 img = plt.imread('./img/pinwheel.png')
